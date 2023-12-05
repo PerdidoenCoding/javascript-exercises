626d8189d587e48b11049863008736660c673107
PerdidoenCoding <willianlinchang@gmail.com>
2023-12-05 14:00:17 +0800

    Completed exercise with while loop.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let newStr = '';
+    let i =0;
+    if (num < 0) {
+        return 'ERROR'
+    } else {
+    while (i < num){
+    i++;
+    newStr += str;
+   }
+    }
 
+   return newStr;
 };
 
 // Do not edit below this line

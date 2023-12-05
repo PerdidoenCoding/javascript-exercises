3ccb32dc7cd17e6d3f51f0324e72e57bd7a36314
PerdidoenCoding <willianlinchang@gmail.com>
2023-12-05 14:02:52 +0800

    Declared newArray and pushed passed values to newA rray.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...nums) {
+    let newArr = [];
+    for(let i = 0; i < arr.length; i++) {
+        if(!nums.includes(arr[i])) {
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
 };
 
 // Do not edit below this line

e2d3ce9617b9e8b22dc5589dde7f29254c011fa5
PerdidoenCoding <willianlinchang@gmail.com>
2023-12-06 11:27:40 +0800

    Simple tempConversion with toFixed to 1 decimal and if statement to check if it is float.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let c = (num - 32) * 5/9;
+  if (c % 1 !== 0) {
+    return parseFloat(c.toFixed(1));
+  } else {
+    return c;
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  let f = num *  9/5 + 32;
+  if (f % 1 !== 0) {
+    return parseFloat(f.toFixed(1));
+  } else {
+    return f;
+  }
 };
 
 // Do not edit below this line

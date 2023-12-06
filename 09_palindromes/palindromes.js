3b84606d91ca5638a4a2a3ce563b1c2b287605ba
PerdidoenCoding <willianlinchang@gmail.com>
2023-12-06 11:33:07 +0800

    Used regex to match only letter and nums then reverse str check if reversed == matched.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let matched = str.toLowerCase().match(/[a-zA-Z0-9]/g).join('');
+    let reversed = matched.split('').reverse().join('');
+    if(matched == reversed) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line

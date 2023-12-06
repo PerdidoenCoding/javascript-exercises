e95d61c728aec7e145179b18a89750c4595ed81e
PerdidoenCoding <willianlinchang@gmail.com>
2023-12-06 21:32:46 +0800

    Declared fibArray with [1,1] and used for loop.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if(n <= 2 && n > 0) {
+        return 1;
+    } else if (n < 0) {
+        return "OOPS";
+    } else {
+      let fibArray = [1, 1]
+        for(let i = 2; i < n; i++) {
+            fibArray.push(fibArray[i-1] + fibArray[i-2]);
+        }
+        return fibArray[n-1];
+    }
 
 };
 

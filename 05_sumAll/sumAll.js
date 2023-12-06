c636f46e94cd8aa45a55f0f687e13f4441473597
PerdidoenCoding <willianlinchang@gmail.com>
2023-12-06 11:26:01 +0800

    Completed exercise with while loop.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    let max = Math.max(num1, num2);
+    let min = Math.min(num1, num2);
+    if(typeof num1 !== 'number' || typeof num2 !== 'number' || max < 0 || min < 0) {
+      return 'ERROR'
+    } else {
+    while(max >= min) {
+        sum += min;
+        min++;
+    }
+    return sum;
+    }
 };
 
 // Do not edit below this line
